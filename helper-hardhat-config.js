3bc5ccb6fc96d85396cd1f04cf4a1263adf40fe1
charlie1099 <cdcharlesworth@gmail.com>
2022-09-09 20:11:25 -0400

    finished test

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -17,7 +17,7 @@ const networkConfig = {
 
 const developmentChains = ["hardhat", "localhost"]
 const DECIMALS = 8
-const INITIAL_ANSWER = 2000
+const INITIAL_ANSWER = 200000000000;
 
 module.exports = {
     networkConfig,
